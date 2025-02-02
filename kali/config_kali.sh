208a8317dab6fa1678881a39c279bf30832abba5
gedemais <geoffrey.demaiso@gmail.com>
2025-02-02 21:37:49 +0100

    kali config added

---
+++ kali/config_kali.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Kali Linux configuration script created by gedemais
+
+# Update package lists and upgrade existing packages
+sudo apt update && sudo apt upgrade -y
+
+# Install openssl
+sudo apt install -y openssl
+
+# Install gpg
+sudo apt install -y gpg
+
+# Install terminator
+sudo apt install -y terminator
+
+# Install asciiquarium for breaks
+sudo apt install -y asciiquarium
+
+# Install MetaSploit
+sudo apt install -y metasploit-framework
+
+# Install nmap
+sudo apt install -y nmap
+
+# Install lynis
+sudo apt install -y lynis
+
+# Copy lynis config
+curl https://raw.github.com/gedemais/config/config_kali/lynisrc > ~/.lynisrc
+
+# Activate it for both bash and zsh
+echo "bash ~/.lynisrc &" >> ~/.bashrc
+echo "zsh ~/.lynisrc &" >> ~/.zshrc
+
+echo "All installations complete !"
